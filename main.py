8047eddbefd9c931f10a3e19d9b02c5037418e6c
Li_ZCh <lee20200905@163.com>
2022-01-30 22:02:05 +0800

    第二版，根据关键词排序

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from unittest import result
-
-
 print("输入文件名称，并确保当前文件夹下没有result.txt文件！")
 name_f = input()
 print('输入飞花令关键词文件名称')
@@ -14,23 +11,22 @@ keywordlist = keyfp.read()
 
 keywordlist = keywordlist.split(' ')
 
-
-this_line = fp.readline() #保存当前这一行的语句
 poem_line = '0' #保存诗词的那一句
 topOFpoem = '0' #保存诗名
 
-while this_line != '':
-    if this_line[0] == '《':
-        topOFpoem = this_line
-        poem_line = fp.readline()
-        while poem_line[0]!='《':
-            for key in keywordlist:
-                if poem_line.find(key) != -1:
-                    resultfp.write(key+'：'+poem_line[0:-1]+'————'+topOFpoem)
+for key in keywordlist:
+    topOFpoem = fp.readline()
+    poem_line = fp.readline()
+    while 1:
+        if poem_line[0] == '《':
+            topOFpoem = poem_line
             poem_line = fp.readline()
-            if poem_line == '':
-                print('结束')
-                resultfp.close()
-                break
-    this_line = poem_line
+        if poem_line.find(key) != -1:
+            resultfp.write(key+'：'+poem_line[0:-1]+'————'+topOFpoem)
+        poem_line = fp.readline()
+        if poem_line == '': 
+            break
+    fp.seek(0)
+resultfp.close()
+print('结束')
 print('END')
